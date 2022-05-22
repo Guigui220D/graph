4992bec37aa00260e9a3ed66f9c78820e0844eac
Guillaume DEREX <gderex8@orange.fr>
2022-05-22 14:13:38 +0200

    Joined dots Tried to make graduations

--- graph_shader.fs
+++ graph_shader.fs
@@ -11,15 +11,38 @@ float map(float value, float inMin, float inMax, float outMin, float outMax) {
 }
 
 float peak(float x, float width) {
-    return width / abs(x);
+    return (abs(x) < width) ? 1 : 0;
 }
 
 void main() {
     vec2 coord = gl_FragCoord.xy;
-    int index = int((coord.x / 800) * 1000);
-    float height = map(data[index], lowerLimit, upperLimit, 0.0, 1.0);
 
-    float i = peak((coord.y / 600) - height, 0.002);
+    if (coord.x >= 50 && coord.x <= 750 && coord.y >= 50 && coord.y <= 550) {
+      float nextpx = coord.x + 1;
+      int index = int(map(coord.x, 50, 750, 0, 997));
+      int index_nextpx = int(map(coord.x + 1, 50, 750, 0, 997));
+      int index_nextnextpx = int(map(coord.x + 2, 50, 750, 0, 997));
+      float avg = 0;
+      for (int i = index; i < index_nextpx; i++) {
+        avg += data[i];
+      }
+      avg /= index_nextpx - index;
+      float avg2 = 0;
+      for (int i = index_nextpx; i < index_nextnextpx; i++) {
+        avg2 += data[i];
+      }
+      avg2 /= index_nextnextpx - index_nextpx;
 
-    color = vec4(i, i, 0.0 / 800, 1.0);
+      float heighta = map(avg, lowerLimit, upperLimit, 0, 600);
+      float heightb = map(avg2, lowerLimit, upperLimit, 0, 600);
+
+      float height = (heighta + heightb) / 2;
+      float width = max(abs(heighta - heightb) / 2, 1);
+
+      float i = peak(map(coord.y, 0, 600, -50, 650) - height, width);
+
+      color = vec4(i, i, 0.0 / 800, i);
+    } else {
+      color = vec4(vec3(0.2), 1.0);
+    }
 }
--- graph_test.c
+++ graph_test.c
@@ -5,9 +5,10 @@
 #include <SFML/Window.h>
 
 void testFn(float * buffer, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        buffer[i] = (float)sin(((float)i) / 30) * i * i;
-        //buffer[i] = 0.1;
+    for (int i = 0; i < size; i++) {
+        float x = (float)i / 1000.f;
+        x *= 10;
+        buffer[i] = sinf(x*x) / (x + 1);
     }
 }
 
@@ -19,7 +20,6 @@ float upperBound(const float * samples, size_t size) {
         if (samples[i] > max)
             max = samples[i];
     }
-    max *= (max > 0) ? 1.1f : 0.9f;
     return max;
 }
 
@@ -31,8 +31,17 @@ float lowerBound(const float * samples, size_t size) {
         if (samples[i] < min)
             min = samples[i];
     }
-    min *= (min < 0) ? 1.1f : 0.9f;
     return min;
+}
+
+float scale(float max) {
+    float base10 = powf(floorf(log10f(max)) - 1, 10);
+    base10 /= 10;
+    return base10;
+}
+
+float map(float value, float inMin, float inMax, float outMin, float outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
 int main() {
@@ -42,6 +51,8 @@ int main() {
     testFn(samples, SIZE);
     float upper = upperBound(samples, SIZE);
     float lower = lowerBound(samples, SIZE);
+    upper += (upper - lower) / 10;
+    lower += (lower - upper) / 10;
 
     printf("Upper: %f, Lower: %f\n", upper, lower);
 
@@ -62,15 +73,64 @@ int main() {
     sfShader_setFloatUniformArray(graph_shader, "data", samples, SIZE);
 
     sfVector2f canvas_size = { 800, 600 };
+    //sfVector2f canvas_pos = { 100, 100};
 
     sfRenderTexture* render_texture = sfRenderTexture_create(800, 600, sfFalse);
+
     sfRectangleShape* canvas = sfRectangleShape_create();
     sfRectangleShape_setSize(canvas, canvas_size);
-    sfRectangleShape_setFillColor(canvas, sfWhite);
+    sfRectangleShape_setFillColor(canvas, sfBlack);
+    //sfRectangleShape_setPosition(canvas, canvas_pos);
+
+    sfVector2f grad_size = { 25, 1 };
+    sfRectangleShape* graduation = sfRectangleShape_create();
+    sfRectangleShape_setSize(graduation, grad_size);
+    sfRectangleShape_setFillColor(graduation, sfGreen);
 
     sfRenderStates states = { .blendMode = sfBlendAlpha, .transform = sfTransform_Identity, .texture = NULL, .shader = graph_shader };
     sfRenderTexture_clear(render_texture, sfBlack);
+    sfRenderTexture_drawRectangleShape(render_texture, canvas, NULL);
     sfRenderTexture_drawRectangleShape(render_texture, canvas, &states);
+    //draw grads
+    // TODO: try to do this in the shader
+    // bad bad bad
+    float g = 0;
+    float grad_space = scale(upper - lower);
+    printf("Grad: %f\n", grad_space);
+    sfVector2f grad_pos = { 25, 0 };
+    if (upper < 0) {
+        while (g > upper)
+            g -= grad_space;
+        while (g > lower) {
+            grad_pos.y = map(g, lower, upper, 50, 550);
+            sfRectangleShape_setPosition(graduation, grad_pos);
+            sfRenderTexture_drawRectangleShape(render_texture, graduation, NULL);
+            g -= grad_space;
+        }
+    } else if (lower > 0) {
+        while (g < lower)
+            g += grad_space;
+        while (g < upper) {
+            grad_pos.y = map(g, lower, upper, 50, 550);
+            sfRectangleShape_setPosition(graduation, grad_pos);
+            sfRenderTexture_drawRectangleShape(render_texture, graduation, NULL);
+            g += grad_space;
+        }
+    } else {
+        while (g < upper) {
+            grad_pos.y = map(g, lower, upper, 50, 550);
+            sfRectangleShape_setPosition(graduation, grad_pos);
+            sfRenderTexture_drawRectangleShape(render_texture, graduation, NULL);
+            g += grad_space;
+        }
+        g = 0;
+        while (g > lower) {
+            grad_pos.y = map(g, lower, upper, 50, 550);
+            sfRectangleShape_setPosition(graduation, grad_pos);
+            sfRenderTexture_drawRectangleShape(render_texture, graduation, NULL);
+            g -= grad_space;
+        }
+    }
     sfRenderTexture_display(render_texture);
 
     sfImage* rendered = sfTexture_copyToImage(sfRenderTexture_getTexture(render_texture));
